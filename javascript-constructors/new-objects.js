0c2f65032134ba4c382757634c58c14f37563578
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-01-25 21:08:05 -0800

    Completed javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var anObject = new Object();
+var anArray = new Array();
+var aFunction = new Function();
+var aString = new String();
+var aNumber = new Number();
+var aBoolean = new Boolean();
+
+console.log('value of anObject:', anObject);
+console.log('typeof anObject:', typeof anObject);
+console.log('value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
+console.log('value of newFunction:', aFunction);
+console.log('typeof newFunction:', typeof aFunction);
+console.log('value of aString:', aString);
+console.log('typeof aString:', typeof aString);
+console.log('value of newNumber:', aNumber);
+console.log('typeof newNumber:', typeof aNumber);
+console.log('value of newBoolean:', aBoolean);
+console.log('typeof newBoolean:', typeof aBoolean);
