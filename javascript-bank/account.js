7f59d3dddbdaa76902ad7bc3390fc44a55d59c82
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-01-31 12:41:02 -0800

    Completed javascript-bank

---
+++ javascript-bank/account.js
@@ -0,0 +1,53 @@
+/* exported Account */
+function Account(number, holder) {
+  this.holder = holder;
+  this.number = number;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+
+    var accountDeposit = new Transaction(
+      this.type = 'deposit',
+      this.amount = amount
+    );
+    this.transactions.push(accountDeposit);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var accountWithdrawal = new Transaction(
+      this.type = 'withdrawal',
+      this.amount = amount
+    );
+    this.transactions.push(accountWithdrawal);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  if (this.transactions.length > 0) {
+    var depositSum = 0;
+    var withdrawSum = 0;
+    var balance = 0;
+    for (var i = 0; i < this.transactions.length; i++) {
+      if (this.transactions[i].type === 'deposit') {
+        depositSum = this.transactions[i].amount + depositSum;
+      } else {
+        withdrawSum = withdrawSum + this.transactions[i].amount;
+      }
+    }
+    balance = depositSum - withdrawSum;
+    return balance;
+  } else {
+    balance = 0;
+    return balance;
+  }
+};
