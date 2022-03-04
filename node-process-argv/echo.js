a9f9bffd9b1adb2e30ee6ad97a6ea6b1861c1739
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-03-04 13:36:38 -0800

    Completed node-process-argv

---
+++ node-process-argv/echo.js
@@ -0,0 +1,8 @@
+// 'Hello!';
+console.log(process.argv[2]);
+// console.log('Hello!');
+// console.log('Command Line Arguments');
+
+// process.argv.forEach((val, index) => {
+//   console.log(`${val}`);
+// });
