83618254d6af1c1ec91b7f42f481dcccf00a902f
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-03-08 08:25:17 -0800

    Delete commented out code

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,33 +1,8 @@
 const fs = require('fs');
 
-for (var i = 0; i < process.argv.length; i++) {
-  if (process.argv[i] === 'cunningham.txt') {
-    fs.readFile('cunningham.txt', 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data);
-    });
-  } else if (process.argv[i] === 'dijkstra.txt') {
-    fs.readFile('dijkstra.txt', 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data);
-    });
-  } else if (process.argv[i] === 'hopper.txt') {
-    fs.readFile('hopper.txt', 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data);
-    });
-  } else if (process.argv[i] === 'hipster-ipsum.txt') {
-    fs.readFile('hipster-ipsum.txt', 'utf8', (err, data) => {
-      if (err) {
-        throw err;
-      }
-      console.log(data);
-    });
+fs.readFile(process.argv.slice(2).toString(), 'utf8', (err, data) => {
+  if (err) {
+    throw err;
   }
-}
+  console.log(data);
+});
