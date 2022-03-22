72bd47c39e518058f956413f7d826476b45f0fc8
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-03-21 19:13:10 -0700

    Completed express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+var nextId = 1;
+var grades = {};
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const prop in grades) {
+    gradesArr.push(grades[prop]);
+  }
+  res.json(gradesArr);
+});
+
+app.post('/api/grades/', (req, res) => {
+  const postedGrade = req.body;
+  const id = nextId;
+  postedGrade.id = id;
+  grades[id] = postedGrade;
+  res.status(201);
+  res.json(postedGrade);
+  nextId++;
+});
+
+app.listen(3000, () => {
+  // console.log('The server is listening');
+});
