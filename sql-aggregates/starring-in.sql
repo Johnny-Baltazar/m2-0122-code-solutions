e7e2373f8bda640ffd9fdbe2138e5fb7f56b921b
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-04-04 19:26:58 -0700

    Delete commented out code

--- sql-aggregates/starring-in.sql
+++ sql-aggregates/starring-in.sql
@@ -1,7 +1,9 @@
 select "genres"."name" as "genre",
-       count("filmId") as "Appearing Lisa Monroe"
+      count(*) as "Appearing Lisa Monroe"
   from "castMembers"
+  join "actors" using ("actorId")
   join "filmGenre" using ("filmId")
   join "genres" using ("genreId")
-  where "actorId" = 178
+  where "firstName" = 'Lisa'
+   and "lastName" = 'Monroe'
   group by "genres"."name";
